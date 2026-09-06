3787b2b2be9e43ee7a0a518f6c0acc5e8c7d1768
Kingmax12 <oyedeletope559@gmail.com>
2026-09-06 14:48:28 +0100

    Refactor tweet.js by moving imports and event listener

--- tweet.js
+++ tweet.js
@@ -67,7 +67,7 @@ Challenge:
     if(tweetInput.value){
         tweetsData.unshift({
             handle: `@Scrimba`,
-            profilePic: `images/scrimbalogo.png`,
+            profilePic: `scrimbalogo.png`,
             likes: 0,
             retweets: 0,
             tweetText: tweetInput.value,
